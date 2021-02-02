04cc0659b470835571e3d0c7caf57eb2b560508a
Nick Vanderweit <nick.vanderweit@gmail.com>
2021-02-02 15:17:41 -0800

    Formatting

--- test/database.py
+++ test/database.py
@@ -8,9 +8,11 @@ class DatabaseTests(unittest.TestCase):
         db = Database()
 
         # Assert that both of our tables are created
-        cursor = db._db.execute("SELECT count() FROM sqlite_schema WHERE type='table' AND (name='openings' OR name='game_dag')")
+        cursor = db._db.execute(
+            "SELECT count() FROM sqlite_schema WHERE type='table' AND (name='openings' OR name='game_dag')")
         self.assertEqual(2, cursor.fetchone()[0])
         self.assertEqual(None, cursor.fetchone())
+
 
 if __name__ == "__main__":
     unittest.main()
