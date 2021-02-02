9eaba629ef77f5f6518a9f337c7d1f97eafe92a6
Nick Vanderweit <nick.vanderweit@gmail.com>
2021-02-02 14:27:23 -0800

    Add first unit test

--- test/database.py
+++ test/database.py
@@ -1,7 +1,16 @@
-import opex.database
 import unittest
 
+from opex.database import Database
 
-class MyTest(unittest.TestCase):
-    def test(self):
-        self.assertTrue(True)
+
+class DatabaseTests(unittest.TestCase):
+    def test_schema(self):
+        db = Database()
+
+        # Assert that both of our tables are created
+        cursor = db._db.execute("SELECT count() FROM sqlite_schema WHERE type='table' AND (name='openings' OR name='game_dag')")
+        self.assertEqual(2, cursor.fetchone()[0])
+        self.assertEqual(None, cursor.fetchone())
+
+if __name__ == "__main__":
+    unittest.main()
