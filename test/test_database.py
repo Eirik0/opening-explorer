e33ca0e7bcf9c45e085dc372b25cf55218286e36
Eirik Ogilvie-Wigley <eowigley@gmail.com>
2021-03-14 18:22:14 -0600

    Add type hints to database test (#48)

--- test/test_database.py
+++ test/test_database.py
@@ -5,12 +5,15 @@ import chess
 from opex import analysis
 from opex import database
 
+import typing
+
 
 class DatabaseTests(unittest.TestCase):
-    # pylint: disable=protected-access
+
     def test_schema(self):
         with database.Database() as db:
             # Assert that both of our tables are created
+            # pylint: disable=protected-access
             cursor = db._db.execute(
                 'SELECT count() FROM sqlite_master WHERE type=\'table\' AND (name=\'openings\' OR name=\'game_dag\')')
             self.assertEqual(2, cursor.fetchone()['count()'])
@@ -19,12 +22,14 @@ class DatabaseTests(unittest.TestCase):
     def test_insert_position(self):
         with database.Database() as db:
             board = chess.Board()
-            id = db.insert_position(analysis.Position(None, board.fen(), None, None, None, None), None)
+            fen = board.fen()  # type: ignore
+            id = db.insert_position(analysis.Position(None, fen, None, None, None, None), None)
             self.assertIsNotNone(id)
 
     def test_get_position(self):
         with database.Database() as db:
             board = chess.Board()
-            insert_position = db.insert_position(analysis.Position(None, board.fen(), None, None, None, None), None)
-            get_position = db.get_position(board.fen())
-            self.assertEqual(insert_position.id, get_position.id)
+            fen = board.fen()  # type: ignore
+            insert_position = db.insert_position(analysis.Position(None, fen, None, None, None, None), None)
+            position = typing.cast(analysis.Position, db.get_position(fen))
+            self.assertEqual(insert_position.id, position.id)
